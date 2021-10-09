fb04fe51e00025dd63549ad7724a262c4e40b38b
Daniel Danielecki <daniel.danielecki@mail.com>
2021-10-09 11:50:59 +0200

    Add: fetching question w/ in/correct answers to an array

--- readme.py
+++ readme.py
@@ -3,7 +3,20 @@ import pandas as pd
 file = open('Test_README.md')
 data = []
 for line in file.readlines():
-   data.append(line.split('###'))  # provide more general splitter
+   if '###' in line:
+      print('question')
+      print(line)
+   elif '- [x]' in line:
+      print('correct answer')
+      print(line)
+   #print(data)
+   elif '- [ ]' in line:
+      print('not correct answer')
+      print(line)
+   if ('###' in line) or ('- [x]' in line) or ('- [ ]' in line):
+      data.append(line)
+
+print(data)
 
 df = pd.DataFrame(data=data)
 df.to_csv('generated.csv')
