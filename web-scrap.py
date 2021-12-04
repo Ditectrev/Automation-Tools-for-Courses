7df60cbecc1721b21841a1b88cd448cd0e211b72
Daniel Danielecki <daniel.danielecki@mail.com>
2021-12-04 16:49:04 +0100

    Add: basic parse of https://www.exam4training.com/amazon/exam-clf-c01-aws-certified-cloud-practitioner-clf-c01/

--- web-scrap.py
+++ web-scrap.py
@@ -4,8 +4,8 @@ import pandas as pd
 import re
 
 # define global parameters
-URL = 'http://www.airlinequality.com/airline-reviews/airasia-x'
-BASE_URL = 'http://www.airlinequality.com'
+URL = 'https://www.exam4training.com/amazon/exam-clf-c01-aws-certified-cloud-practitioner-clf-c01'
+BASE_URL = 'https://www.exam4training.com'
 MASTER_LIST = []
 
 def parse_review(review):
@@ -25,33 +25,34 @@ def parse_review(review):
 
     # get review header
     header = review.find('h2').text
+    print(header)
 
     # get the numerical rating
-    base_review = review.find('div', {'itemprop': 'reviewRating'})
-    if base_review is None:
-        rating = None
-        rating_out_of = None
-    else:
-        rating = base_review.find('span', {'itemprop': 'ratingValue'}).text
-        rating_out_of = base_review.find('span', {'itemprop': 'bestRating'}).text
+    # base_review = review.find('div', {'itemprop': 'reviewRating'})
+    # if base_review is None:
+        # rating = None
+        # rating_out_of = None
+    # else:
+        # rating = base_review.find('span', {'itemprop': 'ratingValue'}).text
+        # rating_out_of = base_review.find('span', {'itemprop': 'bestRating'}).text
 
     # get time of review
-    time_of_review = review.find('h3').find('time')['datetime']
+    # time_of_review = review.find('h3').find('time')['datetime']
 
     # get whether review is verified
-    if review.find('em'):
-        verified = review.find('em').text
-    else:
-        verified = None
+    # if review.find('em'):
+    #     verified = review.find('em').text
+    # else:
+    #     verified = None
 
     # get actual text of review
-    review_text = review.find('div', {'class': 'text_content'}).text
+    review_text = review.find('div', {'class': 'entry-content'}).text
 
     outdf = pd.DataFrame({'header': header,
-                         'rating': rating,
-                         'rating_out_of': rating_out_of,
-                         'time_of_review': time_of_review,
-                         'verified': verified,
+                        #  'rating': rating,
+                        #  'rating_out_of': rating_out_of,
+                        #  'time_of_review': time_of_review,
+                        #  'verified': verified,
                          'review_text': review_text}, index=[0])
 
     return outdf
@@ -69,13 +70,17 @@ def return_next_page(soup):
     next_url - str or None if no next page
     """
     next_url = None
-    cur_page = soup.find('a', {'class': 'active'}, href=re.compile('airline-reviews/airasia'))
-    cur_href = cur_page['href']
+    url = "https://www.exam4training.com/amazon/exam-clf-c01-aws-certified-cloud-practitioner-clf-c01/"
+    req = requests.get(url, headers={"User-Agent": "Chrome"})
+    soup = BeautifulSoup(req.content, 'html.parser')
+    cur_page = soup.find('span', {'class': 'current'})
+    #print(soup.prettify())
     # check if next page exists
-    search_next = cur_page.findNext('li').get('class')
+    search_next = cur_page.findNext('a').get('class')#class="page-numbers"
     if not search_next:
-        next_page_href = cur_page.findNext('li').find('a')['href']
+        next_page_href = cur_page.findNext('a')['href']#findNext('li').find('a')['href']
         next_url = BASE_URL + next_page_href
+        print(next_url)
     return next_url
 
 def create_soup_reviews(url):
@@ -107,7 +112,4 @@ finaldf.shape # (339, 6)
 
 finaldf.head(2)
 finaldf.to_csv('products.csv', index=False, encoding='utf-8')
-print(finaldf)
-# header    rating  rating_out_of   review_text time_of_review  verified
-#"if approved I will get my money back" 1   10  ✅ Trip Verified | Kuala Lumpur to Melbourne. ...    2018-08-07  Trip Verified
-#   "a few minutes error"   3   10  ✅ Trip Verified | I've flied with AirAsia man...    2018-08-06  Trip Verified
+#BeautifulSoup(requests.get("https://www.exam4training.com/amazon/exam-clf-c01-aws-certified-cloud-practitioner-clf-c01/", headers={"User-Agent": "Chrome"}).content, 'html.parser')
