1bc3d7c292306abf56b33fb6a47d37ec1af641b0
Daniel Danielecki <daniel.danielecki@mail.com>
2022-01-09 18:23:19 +0100

    Add: strip undesired characters from questions & answers

--- readme.py
+++ readme.py
@@ -3,18 +3,21 @@ import pandas as pd
 file = open('Test_README.md')
 data = []
 for line in file.readlines():
-   if '###' in line:
-      print('question')
-      print(line)
-   elif '- [x]' in line:
-      print('correct answer')
-      print(line)
-   #print(data)
-   elif '- [ ]' in line:
-      print('not correct answer')
-      print(line)
    if ('###' in line) or ('- [x]' in line) or ('- [ ]' in line):
-      data.append(line)
+      #data.append(line)
+      if '###' in line:
+         print('question')
+         print(line)
+         data.append('question: ' + line.replace("###", "").replace("\n", ""))
+      elif '- [x]' in line:
+         print('correct answer')
+         print(line)
+         data.append('correct answer: ' + line.replace("- [x]", "").replace("\n", ""))
+      #print(data)
+      elif '- [ ]' in line:
+         print('not correct answer')
+         print(line)
+         data.append('not correct answer: ' + line.replace("- [ ]", "").replace("\n", ""))
 
 print(data)
 
