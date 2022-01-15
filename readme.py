000bc6e9ea9b4c8ae995a937384f77ae6955bc88
Daniel Danielecki <daniel.danielecki@mail.com>
2022-01-15 11:56:12 +0100

    Delete: prints & commented out code

--- readme.py
+++ readme.py
@@ -2,24 +2,15 @@ import pandas as pd
 
 file = open('Test_README.md')
 data = []
+
 for line in file.readlines():
    if ('###' in line) or ('- [x]' in line) or ('- [ ]' in line):
-      #data.append(line)
       if '###' in line:
-         print('question')
-         print(line)
          data.append('question: ' + line.replace("###", "").replace("\n", ""))
       elif '- [x]' in line:
-         print('correct answer')
-         print(line)
          data.append('correct answer: ' + line.replace("- [x]", "").replace("\n", ""))
-      #print(data)
       elif '- [ ]' in line:
-         print('not correct answer')
-         print(line)
          data.append('not correct answer: ' + line.replace("- [ ]", "").replace("\n", ""))
-
-print(data)
 
 df = pd.DataFrame(data=data)
 df.to_csv('generated.csv', index=False)
