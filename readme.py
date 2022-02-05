e83b4625d7ab3d6fedcea3535ca10fd666a873b8
Daniel Danielecki <daniel.danielecki@mail.com>
2022-02-05 12:47:29 +0100

    Fix: Udemy format for multiple-choice excl. comma separation

--- readme.py
+++ readme.py
@@ -11,10 +11,10 @@ def generate_answer_string(answers):
          formatted_answer = answer.strip("- [ ] ")
       raw_arr.append(formatted_answer.replace("\n", ""))
       last_item = len(answers) - 1
-      # print(len(answers)-i)
       if len(answers) < 6 and i == last_item:
-         # print(len(answers))
-         raw_arr.append(",")
+         missing_difference = 6 - len(answers)
+         for j in range(missing_difference):
+            raw_arr.append('')
    return raw_arr, answer_arr, len(answer_arr) > 1
 
 
